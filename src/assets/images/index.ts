13a6d854993b72919f9a5b5492b5a53c2039a012
Nicholas <nick.nogue28@outlook.com>
2024-09-07 16:20:04 -0300

    add icons

---
+++ src/assets/images/index.ts
@@ -0,0 +1,19 @@
+/* FILE */
+export { default as addFileIcon } from './icons/add-file.svg'
+export { default as fileIcon } from './icons/file.svg'
+export { default as validFileIcon } from './icons/valid-file.svg'
+export { default as invalidFileIcon } from './icons/invalid-file.svg'
+export { default as fileUnderAttentionIcon } from './icons/file-under-attention.svg'
+
+/* CLIENT */
+export { default as clientsIcon } from './icons/clients.svg'
+export { default as validClientIcon } from './icons/valid-client.svg'
+export { default as invalidClientIcon } from './icons/invalid-client.svg'
+
+/* OTHERS */
+export { default as homeIcon } from './icons/home.svg'
+export { default as filtersIcon } from './icons/filters.svg'
+export { default as pencilIcon } from './icons/pencil.svg'
+export { default as trashCanIcon } from './icons/trash-can.svg'
+export { default as eyeIcon } from './icons/eye.png'
+export { default as blockedEyeIcon } from './icons/blocked-eye.png'
