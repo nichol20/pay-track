47552b78d3e0c1134b7eab9cb1448f6608e3ac5b
Nicholas <nick.nogue28@outlook.com>
2024-09-07 15:42:09 -0300

    add next

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Track pay",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
