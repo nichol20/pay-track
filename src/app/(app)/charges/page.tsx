5c39d67c95641f5c90a065985bc08a216c52f50e
Nicholas <nick.nogue28@outlook.com>
2024-09-16 18:57:22 -0300

    add charges page

---
+++ src/app/(app)/charges/page.tsx
@@ -0,0 +1,57 @@
+"use client"
+import { fileIcon, filterIcon } from '@/assets/images';
+import { ChargeList } from '@/components/ChargeList';
+import { SearchInput } from '@/components/SearchInput';
+import styles from '@/styles/Charges.module.scss';
+import { Charge } from '@/types/charge';
+import Image from 'next/image';
+
+const charges: Charge[] = [
+    {
+        id: 1,
+        cliente_nome: "Andressa",
+        data_venc: "2001-20-12",
+        descricao: "paosodjjap odjsap odjasop dosaihd saoi hdosai hdosia hdoias hdoiah soidh asoi",
+        id_cob: "d8as0d98-da8sd09a8a-312d1s12-s12s212",
+        status: "Paga",
+        valor: 900
+    },
+    {
+        id: 1,
+        cliente_nome: "Andressa",
+        data_venc: "2001-20-12",
+        descricao: "paosodjjap odjsap odjasop dosaihd saoi hdosai hdosia hdoias hdoiah soidh asoi",
+        id_cob: "d8as0d98-da8sd09a8a-312d1s12-s12s212",
+        status: "Pendente",
+        valor: 900
+    },
+    {
+        id: 999,
+        cliente_nome: "Andressa",
+        data_venc: "2001-20-12",
+        descricao: "paosodjjap odjsap odjasop dosaihd saoi hdosai hdosia hdoias hdoiah soidh asoi",
+        id_cob: "d8as0d98-da8sd09a8a-312d1s12-s12s212",
+        status: "Vencida",
+        valor: 900
+    },
+]
+
+export default function ChargesPage() {
+    return (
+        <div className={styles.chargesPage}>
+            <div className={styles.header}>
+                <div className={styles.titleBox}>
+                    <Image src={fileIcon} alt="file" />
+                    <h1>Cobranças</h1>
+                </div>
+                <div className={styles.actions}>
+                    <button className={styles.filterBtn}>
+                        <Image src={filterIcon} alt="filter" />
+                    </button>
+                    <SearchInput onChange={() => { }} delay={400} />
+                </div>
+            </div>
+            <ChargeList rows={charges} />
+        </div>
+    )
+}
