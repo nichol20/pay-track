4ff67e38c8b205c7fa7ec55a3fdc4fba55ee7be6
Nicholas <nick.nogue28@outlook.com>
2024-10-01 17:58:18 -0300

    add indicator animation

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -6,23 +6,67 @@ import { clientsIcon, fileIcon, homeIcon } from '@/assets/images'
 
 import styles from './style.module.scss'
 import { usePathname } from 'next/navigation'
+import { useCallback, useEffect } from 'react'
 
 export const Aside = () => {
     const pathname = usePathname()
 
     const getLinkClass = (path: string) => {
         let className = styles.link
-        if (path === "/") {
-            className += pathname === path ? ` ${styles.active}` : ''
-            return className
-        }
 
-        if (pathname.includes(path)) {
+        if (isCurrentPath(path)) {
             className += ` ${styles.active}`
         }
 
         return className
     }
+
+    const isCurrentPath = useCallback((path: string) => {
+        if (path === "/") {
+            return pathname === path
+        }
+
+        if (pathname.includes(path)) return true
+
+        return false
+    }, [pathname])
+
+    useEffect(() => {
+        const animateIndicator = () => {
+            const indicatorEl = document.querySelector(`.${styles.indicator}`) as HTMLDivElement
+            const transform = window.getComputedStyle(indicatorEl).transform
+
+            let destination = 0
+
+            if (isCurrentPath("/clients")) destination = 140
+            if (isCurrentPath("/charges")) destination = 280
+
+            if (transform !== 'none') {
+                const matrixValues = transform.match(/matrix.*\((.+)\)/)
+                if (matrixValues) {
+                    const values = matrixValues[1].split(',')
+                    const currentTranslateY = parseFloat(values[5])
+
+                    const translateY60 = currentTranslateY > destination ? destination - 20 : destination + 20
+                    const translateY80 = currentTranslateY > destination ? destination + 10 : destination - 10
+
+                    indicatorEl.animate([
+                        { transform: `translateY(${currentTranslateY}px)` },
+                        { transform: `translateY(${translateY60}px)`, offset: 0.6 },
+                        { transform: `translateY(${translateY80}px)`, offset: 0.8 },
+                        { transform: `translateY(${destination}px)` },
+                    ], {
+                        duration: 600,
+                        fill: "forwards",
+                        easing: "cubic-bezier(0, 0, 0.58, 1)", // ease-out
+                        iterations: 1,
+                    })
+                }
+            }
+        }
+
+        animateIndicator()
+    }, [isCurrentPath])
 
     return (
         <aside className={styles.aside}>
